94022318a47a88f49934438a40c3509ae9d3f5ef
Breno Fischer <breno_perricone@hotmai.com>
2022-12-29 14:19:37 -0300

    upload description photos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1>Portfolio</h1>
-    </div>
+    <>
+      {/* <header>
+        <p>header</p>
+      </header> */}
+
+      <main className="App">
+        <div className="description">
+          <div className="description__photo">
+            {/* <img /> */}
+          </div>
+          <div className="description__text">
+            <p>my description</p>
+          </div>
+        </div>
+      </main>
+
+      {/* <footer>
+        <p>footer</p>
+      </footer> */}
+    </>
   );
 }
 
