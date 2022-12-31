08e8ebde4173b6cd50346562bf599e832e4ae0ec
Breno Fischer <breno_perricone@hotmai.com>
2022-12-31 13:54:40 -0300

    nav bar initial style

--- src/routes/navigation/navigaton.components.jsx
+++ src/routes/navigation/navigaton.components.jsx
@@ -1,9 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
     return (
         <>
-            <h1>I am the navigation bar</h1>
+            <div className="nav">
+                <ul className="nav-links">
+                    <Link className="nav-link" to='/projects'>
+                        Home
+                        <div className="nav-link-border"></div>
+                    </Link>
+                    <Link className="nav-link" to='/projects'>
+                        Projects
+                        <div className="nav-link-border"></div>
+                    </Link>
+                    <Link className="nav-link" to='/about'>
+                        About
+                        <div className="nav-link-border"></div>
+                    </Link>
+                    <Link className="nav-link" to='/contact'>
+                        Contact me
+                        <div className="nav-link-border"></div>
+                    </Link>
+                </ul>
+            </div>
             <Outlet />
         </>
     );
