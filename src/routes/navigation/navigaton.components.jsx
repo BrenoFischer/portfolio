7c22c3d7e738d2269e787f847570ca8bbde79fd7
Breno Fischer <breno_perricone@hotmai.com>
2022-12-31 14:16:28 -0300

    move nav link to a component

--- src/routes/navigation/navigaton.components.jsx
+++ src/routes/navigation/navigaton.components.jsx
@@ -2,27 +2,24 @@ import { Outlet, Link } from "react-router-dom";
 
 import './navigation.styles.scss';
 
+const NavLink = ({title}) => {
+    return (
+        <Link className="nav-link" to='/projects'>
+            {title}
+            <div className="nav-link-border"></div>
+        </Link>
+    );
+}
+
 const Navigation = () => {
     return (
         <>
             <div className="nav">
                 <ul className="nav-links">
-                    <Link className="nav-link" to='/projects'>
-                        Home
-                        <div className="nav-link-border"></div>
-                    </Link>
-                    <Link className="nav-link" to='/projects'>
-                        Projects
-                        <div className="nav-link-border"></div>
-                    </Link>
-                    <Link className="nav-link" to='/about'>
-                        About
-                        <div className="nav-link-border"></div>
-                    </Link>
-                    <Link className="nav-link" to='/contact'>
-                        Contact me
-                        <div className="nav-link-border"></div>
-                    </Link>
+                    <NavLink title={"Home"} />
+                    <NavLink title={"Projects"} />
+                    <NavLink title={"About"} />
+                    <NavLink title={"Contact me"} />
                 </ul>
             </div>
             <Outlet />
