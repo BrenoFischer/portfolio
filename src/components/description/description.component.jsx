830f26af1cb98571aaaba56d33de6bb9074f1fee
Breno Fischer <breno_perricone@hotmai.com>
2023-01-05 21:32:16 -0300

    insert MUI and Icons on description

--- src/components/description/description.component.jsx
+++ src/components/description/description.component.jsx
@@ -1,8 +1,31 @@
 import descriptionPhoto from '../../assets/description-photo1.1.png';
 
+import IconButton from '@mui/material/IconButton';
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+
+
 import './description.styles.scss';
 
 const Description = () => {
+  const DefineIconButton = (props) => {
+    return (
+      <IconButton
+        sx={{
+          color: "rgb(255, 113, 91)",
+          "&:hover": {
+              color: "white"
+            },
+          transition: "all .5s"
+          }}
+        >
+        {props.children}
+      </IconButton>
+    )
+  }
+
     return (
         <section className="description">
           <div className="description__text">
@@ -12,19 +35,32 @@ const Description = () => {
             <p className='description__text-paragraph'>in touch with multiple techs - from machine learning to mobile dev.</p>
           </div>
           <div className="description__photo-box">
-            <div class="description__photo-box-bubble">Hi!</div>
-            <div class="description__photo-box-pointer"></div>
-            <div class="description__photo-box-bubble bubble-2">Welcome to my portfolio</div>
-            <div class="description__photo-box-pointer pointer-2"></div>
-            <div class="description__photo-box-bubble bubble-2">Welcome to my portfolio</div>
-            <div class="description__photo-box-pointer pointer-2"></div>
-            <div class="description__photo-box-bubble bubble-3">Explore my projects!</div>
-            <div class="description__photo-box-pointer pointer-3"></div>
-            <div class="description__photo-box-bubble bubble-4">Read more about my journey</div>
-            <div class="description__photo-box-pointer pointer-4"></div>
-            <div class="description__photo-box-bubble bubble-5">You can easily "Contact me"</div>
-            <div class="description__photo-box-pointer pointer-5"></div>
+            <div className="description__photo-box-bubble">Hi!</div>
+            <div className="description__photo-box-pointer"></div>
+            <div className="description__photo-box-bubble bubble-2">Welcome to my portfolio</div>
+            <div className="description__photo-box-pointer pointer-2"></div>
+            <div className="description__photo-box-bubble bubble-2">Welcome to my portfolio</div>
+            <div className="description__photo-box-pointer pointer-2"></div>
+            <div className="description__photo-box-bubble bubble-3">Explore my projects!</div>
+            <div className="description__photo-box-pointer pointer-3"></div>
+            <div className="description__photo-box-bubble bubble-4">Read more about my journey</div>
+            <div className="description__photo-box-pointer pointer-4"></div>
+            <div className="description__photo-box-bubble bubble-5">You can easily "Contact me"</div>
+            <div className="description__photo-box-pointer pointer-5"></div>
+            
             <img className="description__photo" src={descriptionPhoto} alt="Description" />
+            
+            <div className='description__icons'>
+              <DefineIconButton>
+                <GitHubIcon sx={{fontSize: 40}} />
+              </DefineIconButton>
+              <DefineIconButton>
+                <LinkedInIcon sx={{fontSize: 40}} />
+              </DefineIconButton>
+              <DefineIconButton>
+                <EmailIcon sx={{fontSize: 40}} />
+              </DefineIconButton>
+            </div>
           </div>
         </section>
     );
