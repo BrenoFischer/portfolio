faae6c7c9df8dc5218931709217dbd3c40789cbc
Breno Fischer <breno_perricone@hotmai.com>
2023-01-09 14:35:30 -0300

    add projects images, titles and stacks

--- src/components/projects/projects.component.jsx
+++ src/components/projects/projects.component.jsx
@@ -1,24 +1,28 @@
 import Button from '../button/button.component';
 
+import groundstationImage from '../../assets/projects-images/groundstation.gif'
+import vipImage from '../../assets/projects-images/vip.png';
+import reactImage from '../../assets/projects-images/react.gif';
+import travelogImage from '../../assets/projects-images/travelog.png';
+import sassImage from '../../assets/projects-images/sass.gif';
+
 import './projects.styles.scss';
 
-const Project = () => {
+const Project = ({ image, title, descriptionText, stacks }) => {
     return (
         <div className='project'>
-            <div className='project__photo-box'>
-                <div className='project__photo'></div>
-            </div>
+            <div className='project__photo-box' style={{backgroundImage: `url(${image})`}}></div>
             <div className='project__description'>
-                <h2 className='project__title'>UAV Groundstation</h2>
+                <h2 className='project__title'>{title}</h2>
                 <div className='project__stacks'>
-                    <div className='project__stack'>JS</div>
-                    <div className='project__stack'>HTML5</div>
-                    <div className='project__stack'>CSS3</div>
+                    {stacks.map((stack) => {
+                        return <div className='project__stack'>{stack}</div>
+                    })}
+                    {/* <div className='project__stack'>HTML5</div>
+                    <div className='project__stack'>CSS3</div> */}
                 </div>
                 <div className='project__description-text'>
-                    <p>A big drescription about the project I made, stating why this project is amazing
-                        and introducing the project theme, the blablablabla blab
-                    </p>
+                    <p>{descriptionText}</p>
                 </div>
                 <div className='project__btn-box'>
                     <Button buttonText={"View project"} />
@@ -29,6 +33,12 @@ const Project = () => {
 }
 
 const Projects = () => {
+    const stacksGS = ["Javascript", "HTML5", "CSS3", "Python", "Django","Websockets", "ESP32"];
+    const stacksVip = ["Javascript", "HTML5", "CSS3", "React", "Sass"];
+    const stacksReact = ["Javascript", "Typescript", "HTML5", "CSS3", "React", "Redux", "Firebase", "Styled-Components", "Sass", "GraphQL"];
+    const stacksTravelog = ["Dart", "Flutter", "Firebase", "MaterialUI"];
+    const stacksSass = ["Javascript", "HTML5", "CSS3", "Sass"];
+
     return (
         <section className='projects-section'>
             <div className='projects-section__title-box'>
@@ -36,8 +46,36 @@ const Projects = () => {
                 <div className='projects-section__title-border'></div>
             </div>
             <div className='projects-box'>
-                <Project />
-                <Project />
+                <Project 
+                    image={groundstationImage}
+                    title={"UAV Groundstation"}
+                    descriptionText={"Text text a big description text to explain and introduce the project. Make sure to don't make it so big, but also has all the contect necessary to reader understand what this is about"}
+                    stacks={stacksGS}
+                />
+                <Project 
+                    image={vipImage}
+                    title={"VIP Beauty Rooms"}
+                    descriptionText={"Text text a big description text to explain and introduce the project. Make sure to don't make it so big, but also has all the contect necessary to reader understand what this is about"}
+                    stacks={stacksVip}
+                />
+                <Project 
+                    image={reactImage}
+                    title={"Crown Clothing"}
+                    descriptionText={"Text text a big description text to explain and introduce the project. Make sure to don't make it so big, but also has all the contect necessary to reader understand what this is about"}
+                    stacks={stacksReact}
+                />
+                <Project 
+                    image={travelogImage}
+                    title={"Travelog"}
+                    descriptionText={"Text text a big description text to explain and introduce the project. Make sure to don't make it so big, but also has all the contect necessary to reader understand what this is about"}
+                    stacks={stacksTravelog}
+                />
+                <Project 
+                    image={sassImage}
+                    title={"Natours"}
+                    descriptionText={"Text text a big description text to explain and introduce the project. Make sure to don't make it so big, but also has all the contect necessary to reader understand what this is about"}
+                    stacks={stacksSass}
+                />
             </div>
         </section>
     );
