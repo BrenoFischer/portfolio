b93d7af098850729a26f91409b28eb8bc5b87eda
Breno Fischer <breno_perricone@hotmai.com>
2023-01-11 17:43:23 -0300

    insert skills + skills style on about section

--- src/routes/about/about.component.jsx
+++ src/routes/about/about.component.jsx
@@ -1,19 +1,21 @@
 import './about.styles.scss';
 
 import { IconContext } from 'react-icons';
-import { FaJs } from "react-icons/fa";
+import { FaJs, FaHtml5, FaCss3Alt, FaReact, FaSass } from "react-icons/fa";
+import { TbBrandReactNative } from "react-icons/tb";
+import { SiStyledcomponents, SiRedux, SiFlutter, SiDart, SiTypescript, SiFirebase } from "react-icons/si";
 
 const SkillIcon = ({icon, title}) => {
     return (
-        <>
-            <IconContext.Provider value={{ color: "white", size: "5rem", className: "global-class-name" }}>
+        <div className='about-section__skill'>
+            <IconContext.Provider value={{ size: "5rem", className: "global-class-name" }}>
                 <div className='about-section__skill-icon'>
                     {icon}
                 </div>
             </IconContext.Provider> 
             
             <div className='about-section__skill-title'>{title}</div>
-        </>
+        </div>
     );
 }
 
@@ -33,6 +35,17 @@ const About = () => {
                 </div>
                 <div className='about-section__skills-box'>
                     <SkillIcon icon={ <FaJs /> } title={"Javascript"} />
+                    <SkillIcon icon={ <FaHtml5 /> } title={"HTML5"} />
+                    <SkillIcon icon={ <FaCss3Alt /> } title={"CSS3"} />
+                    <SkillIcon icon={ <FaReact /> } title={"React.js"} />
+                    <SkillIcon icon={ <TbBrandReactNative /> } title={"React Native"} />
+                    <SkillIcon icon={ <FaSass /> } title={"Sass"} />
+                    <SkillIcon icon={ <SiStyledcomponents /> } title={"Styled-Components"} />
+                    <SkillIcon icon={ <SiRedux /> } title={"Redux"} />
+                    <SkillIcon icon={ <SiFlutter /> } title={"Flutter"} />
+                    <SkillIcon icon={ <SiDart /> } title={"Dart"} />
+                    <SkillIcon icon={ <SiFirebase /> } title={"Firebase"} />
+                    <SkillIcon icon={ <SiTypescript /> } title={"Typescript"} />
                 </div>
             </div>
         </section>
